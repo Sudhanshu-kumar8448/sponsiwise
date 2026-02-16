0443be10f6f2660e27aa589ee7242d0523955d91
Sudhanshu Kumar <sudhanshukumar8448@gmail.com>
2026-02-16 17:58:28 +0530

    After fixing the seed data

---
+++ use-node20.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run this before npm commands: source use-node20.sh
+# Or: export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
+export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
+echo "Using Node $(node -v)"
